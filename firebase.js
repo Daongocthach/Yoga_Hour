35859cb91c203f27f2e7cce7c2e4245709da90ba
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2024-08-13 11:48:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,19 +7,6 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: 'AIzaSyBTlmXHrQ-JiWTghnu9kn0eY9iSGF3Z9d8',
-    // authDomain: 'yoga-hour.firebaseapp.com',
-    // projectId: 'yoga-hour',
-    // storageBucket: 'yoga-hour.appspot.com',
-    // messagingSenderId: '172709730321',
-    // appId: '1:172709730321:web:5bb9ed31cb1036f45ab9b0'
-
-    apiKey: 'AIzaSyAQ4AiGxztxG3Cp_Ds9znCIBvc6Rt_VhHI',
-    authDomain: 'test-yoga-hour.firebaseapp.com',
-    projectId: 'test-yoga-hour',
-    storageBucket: 'test-yoga-hour.appspot.com',
-    messagingSenderId: '664662925901',
-    appId: '1:664662925901:web:7856575b025c45a2def204'
 }
 
 // Initialize Firebase
